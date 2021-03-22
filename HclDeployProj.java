2dd952a3fa7d493d913151cf7a5b82bf0a309bd8
Gouravmhatre <78531105+Gouravmhatre@users.noreply.github.com>
2021-03-22 07:53:55 +0530

    Update HclDeployProj.java

--- HclDeployProj.java
+++ HclDeployProj.java
@@ -7,11 +7,11 @@ import java.io.FileWriter;
      String data = "This is the data in the output file";
      try {
        // Creates a Writer using FileWriter
-       FileWriter output = new FileWriter("c:\\Javatest\\newFile.txt");
+       FileWriter output = new FileWriter("c:\\Jenkinswithjava\\newFile.txt");
 
        // Writes string to the file
        output.write(data);
-       System.out.println("Data is written to the file.");
+       System.out.println("successfully deployed.");
 
        // Closes the writer
        output.close();
